2de5ad50ce34fb9d6d717f1c2d3a1b06668b3138
Oliya23 <ibaconsultancy1@gmail.com>
2026-09-16 20:55:33 +0500

    optional: regex support in find

--- user/find.c
+++ user/find.c
@@ -3,7 +3,44 @@
 #include "user/user.h"
 #include "kernel/fs.h"
 #include "kernel/param.h"
+int match(char*, char*);
+int matchhere(char*, char*);
+int matchstar(int, char*, char*);
 
+int match(char *re, char *text)
+{
+  if(re[0] == '^')
+    return matchhere(re+1, text);
+  do{
+    if(matchhere(re, text))
+      return 1;
+  }while(*text++ != '\0');
+  return 0;
+}
+
+int
+matchhere(char *re, char *text)
+{
+  if(re[0] == '\0')
+    return 1;
+  if(re[1] == '*')
+    return matchstar(re[0], re+2, text);
+  if(re[0] == '$' && re[1] == '\0')
+    return *text == '\0';
+  if(*text!='\0' && (re[0]=='.' || re[0]==*text))
+    return matchhere(re+1, text+1);
+  return 0;
+}
+
+int
+matchstar(int c, char *re, char *text)
+{
+  do{
+    if(matchhere(re, text))
+      return 1;
+  }while(*text!='\0' && (*text++==c || c=='.'));
+  return 0;
+}
 char*
 fmtname(char *path)
 {
@@ -61,7 +98,7 @@ find(char *path, char *name, char **cmdargv, int cmdargc)
 
   switch (st.type) {
   case T_FILE:
-    if (strcmp(fmtname(path), name) == 0) {
+    if (match(name, fmtname(path))) {
       if (cmdargc > 0)
         runexec(cmdargv, cmdargc, path);
       else
@@ -90,7 +127,7 @@ find(char *path, char *name, char **cmdargv, int cmdargc)
         continue;
       }
 
-      if (strcmp(fmtname(buf), name) == 0) {
+      if (match(name, fmtname(buf))) {
         if (cmdargc > 0)
           runexec(cmdargv, cmdargc, buf);
         else
